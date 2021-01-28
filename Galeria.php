e46a6a8678abac704df7e046d47126de80e5bf52
Fazt <contact@faztweb.com>
2021-01-28 16:08:26 -0500

    comit1

---
+++ Galeria.php
@@ -0,0 +1,15 @@
+<html>
+<head>
+
+</head>
+<body>
+   <h1>Galeria</h1>>
+    <ul>
+   <li class="menu_ _item"><a href="Inicio.html" class="menu_ _link menu_ _link- -select">Inicio</a></li>
+ <li class="menu_ _item"><a href="Nosotros.html" class="menu_ _link">Nosotros</a></li>
+ <li class="menu_ _item"><a href="Galeria.html" class="menu_ _link">Galeria</a></li>
+ <li class="menu_ _item"><a href="Contacto.html" class="menu_ _link">Contacto</a></li>
+</ul>
+<p>Esta pagina Galeria</p>
+</body>
+</html>
---
+++ diseño1.php
@@ -0,0 +1,148 @@
+
+<!DOCTYPE html>
+ <html lang="es">
+ <cabeza>
+
+
+ <meta charset="UTF-8">
+
+ <title>Farmacity</title>
+
+ <meta name="viewport" content="width=device-width,user-scalable=yes, initial-scale=1.0, maximum-scale=3.0, minimum-scale=1.0">
+<link rel="stylesheet" href="css/trabajo.css"/>
+
+ <link href="https://file.myfontastic.com/AgpgEL5stynTCJXv2mJuD8/icons.css" rel="stylesheet"/>
+</cabeza>
+
+<style>
+ div, span { border :0px solid black; }
+ div { background-color: turquoise; }
+ span { background-color:white; }
+ p { font-size:14pt; }
+h1 { font-size:240%; }
+h2 { font-size:200%; }
+h3 { font-size:160%; }
+h4 { font-size:120%; } 
+
+ </style> 
+<style type="text/css">
+body{
+color: gray;
+background-color:
+}
+#seccion:{
+ font-size:60px;
+ text-align: center;
+ background-image: url(https://cdn.pixabay.com/photo/2018/07/15/10/44/dna-3539309_960_720.jpg);
+ background-size: contain;
+}
+</style>
+<cuerpo>
+ <header class="main-header">
+ <div class="container container--flex">
+  <div class="logo-container">
+  <h1 class="logo">Farmacity1</h>
+  </div>
+  <div class="main-header_ _contacInfo">
+  <div class="main-header_ _contacInfo_ _phone">
+  <span class="icon-phone">999-999-999 </span>
+  </div>
+    <div class="main-header_ _contacInfo_ _address">
+    <span class="icon-location">Los pinos 123 - Andahuaylas,Peru </span>
+
+
+  </p>
+  </div>
+</header>
+<nav class="main-nav">
+<div class="container  container--flex"></div>
+ <span class="icon-menu" id="icon-menu"></span>
+ <ul class="menu"id="menu">
+
+<li class="menu_ _item"><a href="Productos.html " class="menu_ _link">Productos</a>
+ <li class="menu_ _item"><a href="Nosotros.html" class="menu_ _link">Nosotros</a></li>
+ <li class="menu_ _item"><a href="Galeria.html" class="menu_ _link">Galeria</a></li>
+ <li class="menu_ _item"><a href="Contacto.html" class="menu_ _link">Contacto</a></li>
+ </ul>
+ <div class="social-icon">
+ <a href="" class="social-icon_ _link"><span class="icon-facebook"></span></a>
+  <a href="" class="social-icon_ _link"><span class="icon-twitter"></span></a>
+  <a href="" class="social-icon_ _link"><span class="icon-mail"></span></a>
+ </div>
+  </div>
+</nav>
+<section class="banner">
+<img src="img/banner.jpeg" alt="" class="banner_ _img">
+<div class="banner_ _content">Los mejores productos lo encuentras aqui</div>
+</section>
+<main class="main">
+  <section class="group group--color">
+    <div class="container">
+      <h2 class="main_ _title">Bienvenida a su Farmacity</h2>
+      <p class= "main_ _txt">vjvvvv </p>
+    </div>
+  </section>
+  <section class="group main_ _about_ _description">
+    <div class="container container- -flex">
+    <div class="column column- -50">
+    <img src="img/food1.jpg" alt="">
+  </div>
+  <div class="column column- -50">
+  <h3 class= "column_ _title">Expertise in industrial solution </h3>
+  <p class="column_ _txt">loream dsahaha </p>
+  <a href="" class="btn btn--contact">Contact</a>
+</div>
+</div>
+</section>
+<section class="group today-special">
+<h2 class="today-special_ _title">Especial de hoy </h2>
+<div class="container container_ _flex">
+<div class="column column- -50-25">
+<img src="img/plat1.jpg" alt="" class="today-special_ _img">
+<div class="today-special_ _title">Especial1</div>
+<div class="today-special_ _price">S\30.00</div>
+</div>
+<div class="column column- -50-25">
+<img src="img/plat2.jpeg" alt="" class="today-special_ _img">
+<div class="today-special_ _title">Especial2</div>
+<div class="today-special_ _price">S\10.00</div>
+</div>
+<div class="column column- -50-25">
+<img src="img/PONDS.jpg" alt="" class="today-special_ _img">
+<div class="today-special_ _title">Especial3</div>
+<div class="today-special_ _price">s\20.00</div>
+</div>
+<div class="column column- -50-25">
+<img src="img/plat4.jpeg"alt="" class="today-special_ _img">
+<div class="today-special_ _title">Especial4</div>
+<div class="today-special_ _price">s\39.00</div>
+</div>
+</div>
+</section>
+<section class="group today-special">
+
+     </main>
+     <footer class="main-footer">
+     <div class="container container--flex">
+     <div class="column column-33">
+      <h2 class="column_ _title">¿Por que visitarnos?</h2>
+       <p class="column_ _txt">lorem  sajskskccdgcdsgcjdshdshjhjdsvv</p>
+       </div>
+     <div class="column column-33 ">
+     <h2 class="column_ _title">contactonos</h2>
+       <p class="column_ _txt">Urb  las dunas-   </p>
+       <p class="column_ _txt">telefono:999-999-999</p>
+       <p class="column_ _txt">consulta@Farmacity.com</p>
+       </div>
+          <div class="column column-33">
+           <h2 class="column_ _title">siguenos en nuestras redes</h2>
+           <p class="column_ _txt"><a href="" class="icon-facebook">facebook</a></p>
+           <p class="column_ _txt"><a href="" class="icon-twitter">siguenos en twitter</a></p>
+           <p class="column_ _txt"><a href="" class="icon-Youtube">visita nuestro local</a></p>
+            </div>
+            <p class="copy">@ 2018 Your Farmacity| Todos los derechos reservados</p>
+            </div>
+          </footer>
+
+</cuerpo>
+</html>
